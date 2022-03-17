cad19fe46b1e24366088206989ecff1897bcd5f6
Bharath <bharath.sivakumar@freshworks.com>
2022-03-18 01:24:54 +0530

    random number synthesizer added

--- velloredle/src/main/java/org/project/velloredle/Util.java
+++ velloredle/src/main/java/org/project/velloredle/Util.java
@@ -4,7 +4,13 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.google.gson.Gson;
 
+import java.util.Random;
+
 public class Util {
+	
+	private static final int MAX_VALUE = 5757;
+	
+	private static final ThreadLocal<Random> THREAD_LOCAL_RANDOM = ThreadLocal.withInitial(Random::new);
 
 	 public static void logEnvironment(Object event, Context context, Gson gson)
 	  {
@@ -16,4 +22,14 @@ public class Util {
 	    logger.log("EVENT: " + gson.toJson(event));
 	    logger.log("EVENT TYPE: " + event.getClass().toString());
 	  }
+	 
+	 // generate a pseudo random number between 0(inclusive) and 5757(exclusive)
+	 public static int getRandomWord(long date) {
+		 
+		Random random = THREAD_LOCAL_RANDOM.get();
+		random.setSeed(date);
+		int lineNumber = random.nextInt(MAX_VALUE);
+		return lineNumber;
+		 
+	 }
 }
--- velloredle/src/main/java/org/project/velloredle/service/impl/VelloredleServiceImpl.java
+++ velloredle/src/main/java/org/project/velloredle/service/impl/VelloredleServiceImpl.java
@@ -1,23 +1,53 @@
 package org.project.velloredle.service.impl;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.Properties;
+import java.util.Optional;
+
+import org.project.velloredle.Util;
 
 import org.project.velloredle.service.VelloredleService;
 
+
 public class VelloredleServiceImpl implements VelloredleService {
 
-	public String getWord(Date date) {
+	public String getWord(Date today) {
 		
-		Properties properties = new Properties();
+		String wordOfTheDay = null;
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(today);
+		int date = calendar.get(Calendar.DAY_OF_MONTH);
+		int month = calendar.get(Calendar.MONTH)+1;
+		int year = calendar.get(Calendar.YEAR);
+		String dateString = Integer.toString(date)+Integer.toString(month)+Integer.toString(year);
+		int randomLineNumber = getRandomWord(Long.parseLong(dateString));
+		FileReader fileReader;
 		try {
-			properties.load(this.getClass().getResourceAsStream("/application.properties"));
-			String word = properties.getProperty("word");
-			return word;
-		} catch (IOException e) {
+			fileReader = new FileReader("/fiveletter.txt");
+		} catch (FileNotFoundException e1) {
 			return null;
 		}
+		
+		
+		try {
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			Optional<String> wordOfTheDayObj = bufferedReader.lines().skip(randomLineNumber).limit(1).findAny();
+			bufferedReader.close();
+			if(wordOfTheDayObj.isPresent()) {
+				wordOfTheDay = wordOfTheDayObj.get();
+			}
+			return wordOfTheDay;
+		} catch (IOException e1) {
+			return null;
+		}
+	}
+	
+	public int getRandomWord(long seed) {
+		return Util.getRandomWord(seed);
 	}
 
 }
